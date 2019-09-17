b8e50c009ebd875d7c192b1ec135f2d36f546fa6
weilong.wang <weilong.wang@jiamiantech.com>
2019-09-17 09:39:06 +0800

    init

---
+++ micro-service/service-account/src/main/java/org/example/AccountStarter.java
@@ -0,0 +1,12 @@
+package org.example;
+
+/**
+ * @description:
+ * @author: alonwang
+ * @create: 2019-09-17 09:26
+ **/
+public class AccountStarter {
+    public static void main(String[] args) {
+        System.out.println("Account starter");
+    }
+}
---
+++ micro-service/service-user/src/main/java/org/example/UserStarter.java
@@ -0,0 +1,12 @@
+package org.example;
+
+/**
+ * @description:
+ * @author: alonwang
+ * @create: 2019-09-17 09:27
+ **/
+public class UserStarter {
+    public static void main(String[] args) {
+        System.out.println("User Starter");
+    }
+}
